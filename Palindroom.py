9d8f02ef069d1fec7017d595fb77e61d81e0323b
r0waldo <oualid.bouzraa@student.hu.nl>
2020-02-07 14:55:23 +0100

    Formatieve Opdracht

--- Palindroom.py
+++
@@ -1,7 +0,0 @@
-def palindroom():
-    woord = str(input('Voer een woord in: '))
-    if woord == woord[::-1]:
-        print(woord, 'is een palindroom.')
-    else:
-        print(woord, 'is geen palindroom.')
-palindroom()
--- Tekstcheck.py
+++
@@ -1,8 +0,0 @@
-def tekstcheck():
-    zin1= str(input('De string is: '))
-    zin2= str(input('De string is: '))
-    for i in range(len(zin1)):
-        if zin1[i] != zin2[i]:
-            print('Het eerste verschil zit op index:' + str([i]))
-
-tekstcheck()
