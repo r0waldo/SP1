0bf5ac90ac09b37d707fc2128d98711ae9142311
r0waldo <oualid.bouzraa@student.hu.nl>
2020-02-07 15:40:28 +0100

    Eind Product

---
+++ Formatieve Opdracht 1/Cyclisch verschuiven.py
@@ -0,0 +1,7 @@
+def verschuif(ch, n):
+    ch = int(input('Geef een bitwaarde: '))
+    if n > 0:
+
+    elif n < 0:
+
+verschuif()
--- Formatieve Opdracht 1/FizzBuzz.py
+++ Formatieve Opdracht 1/FizzBuzz.py
@@ -1,13 +1,14 @@
 def FizzBuzz():
     for fizzbuzz in range(1, 100):
         if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-            print("fizzbuzz")
+            print("FizzBuzz")
             continue
         elif fizzbuzz % 3 == 0:
-            print("fizz")
+            print("Fizz")
             continue
         elif fizzbuzz % 5 == 0:
-            print("buzz")
+            print("Buzz")
             continue
         print(fizzbuzz)
 FizzBuzz()
+# http://www.blog.pythonlibrary.org/2019/09/18/python-code-kata-fizzbuzz/
