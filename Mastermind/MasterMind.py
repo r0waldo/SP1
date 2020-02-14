82c7cd7d4774ac509f563acd51acab4f6c989612
r0waldo <oualid.bouzraa@student.hu.nl>
2020-02-14 13:08:13 +0100

    MasterMind

--- Mastermind/MasterMind.py
+++ Mastermind/MasterMind.py
@@ -1,22 +1,68 @@
+import random
+
+def gokker():
+    n1 = random.randint(0, 6)
+    n2 = random.randint(0, 6)
+    n3 = random.randint(0, 6)
+    n4 = random.randint(0, 6)
+    code = []
+    code.append(n1)
+    code.append(n2)
+    code.append(n3)
+    code.append(n4)
+    print(code)
+
+    while True:
+        gok1 = int(input('Gok voor cijfer 1: '))
+        gok2 = int(input('Gok voor cijfer 2: '))
+        gok3 = int(input('Gok voor cijfer 3: '))
+        gok4 = int(input('Gok voor cijfer 4: '))
+        gok = []
+        gok.append(gok1)
+        gok.append(gok2)
+        gok.append(gok3)
+        gok.append(gok4)
+        tel = 0
+        for x in gok:
+            for y in code:
+                if x == y:
+                    tel = tel + 1
+                    break
+        print(tel, "cijfers goed geraden."
+                   "")
+
+        if gok == code:
+            print('Goed geraden! Je bent een echte MasterMind.')
+            break
+        else:
+            print('Probeer opnieuw.')
+
+
+def regels():
+    print('1. Je voert een 4-cijferige code in. \n'
+          '2. Heb je een getal die voorkomt in de geheime code zie je # \n'
+          '3. Heb je een getal op de juiste plek ingevoerd zie je $')
+
+# def gamemaster():
+
+
 def menu():
     print("************MASTERMIND**************")
 
     choice = input("""
     A: GameMaster
     B: Gokker
-    Q: Stoppen
+    C: Regels
 
-    Please enter your choice: """)
+    Wat is je keuze: """)
 
     if choice == "A" or choice =="a":
         gamemaster()
     elif choice == "B" or choice =="b":
         gokker()
-    elif choice=="Q" or choice=="q":
-        sys.exit
+    elif choice == 'C' or choice == 'c':
+        regels()
     else:
-        print("You must only select either A,B or Q.")
-        print("Please try again")
+        print("Je kunt alleen kiezen tussen A, B of C.")
+        print("Probeer opnieuw")
 menu()
-
-def gokker():
